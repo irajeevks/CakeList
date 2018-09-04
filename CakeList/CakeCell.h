947e78954c72fb96d4d7efe9db9ff8dbdfd8109b
Rajeev <rajeev@mindfury.co.uk>
2018-09-04 15:28:45 +0100

    first commit

---
+++ CakeList/CakeCell.h
@@ -0,0 +1,13 @@
+//
+//  CakeCell.h
+//  CakeList
+//
+//  Created by RAjeev Singh on 20/02/2018.
+//  Copyright © 2018 RAjeev Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CakeCell : UITableViewCell
+
+@end
---
+++ CakeList/CakeCell.m
@@ -0,0 +1,24 @@
+//
+//  CakeCell.m
+//  CakeList
+//
+//  Created by RAjeev Singh on 20/02/2018.
+//  Copyright © 2018 RAjeev Singh. All rights reserved.
+//
+
+#import "CakeCell.h"
+
+@implementation CakeCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
